aee1d96bc56af74d0de5d9b11a7cc78136f0b3be
handiko <handikogesang@gmail.com>
2018-12-22 06:39:34 +0700

    Recompile

--- Arduino-Sketch/Connect_to_Specific_Access_Point/Connect_to_Specific_Access_Point.ino
+++ Arduino-Sketch/Connect_to_Specific_Access_Point/Connect_to_Specific_Access_Point.ino
@@ -8,11 +8,18 @@
 const char* ssid = "My-WiFi";
 const char* password = "Metallica";
 
+bool led = 1;
+
 void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, led);
+  
   Serial.begin(115200);
   delay(10);
   Serial.println('\n');
 
+  digitalWrite(LED_BUILTIN, 0);
+  
   WiFi.begin(ssid, password);
   Serial.print("Connecting to ");
   Serial.print(ssid);
@@ -30,8 +37,15 @@ void setup() {
   Serial.println("Connection established!");
   Serial.print("IP address:\t");
   Serial.println(WiFi.localIP());
+
+  digitalWrite(LED_BUILTIN, 1);
 }
 
 void loop() {
-  
+  for(int i=0;i<4;i++) {
+    led ^= 1;
+    digitalWrite(LED_BUILTIN, led);
+    delay(75);
+  }
+  delay(2000);
 }
