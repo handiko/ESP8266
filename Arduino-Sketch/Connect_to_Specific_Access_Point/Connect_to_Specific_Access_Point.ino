c84f6c592a4a3ddfcf7716219dc875a42d79a273
handiko <handikogesang@gmail.com>
2018-12-22 06:16:51 +0700

    Adding Connect to Specific Access Point

--- Arduino-Sketch/Connect_to_Specific_Access_Point/Connect_to_Specific_Access_Point.ino
+++ Arduino-Sketch/Connect_to_Specific_Access_Point/Connect_to_Specific_Access_Point.ino
@@ -9,7 +9,27 @@ const char* ssid = "My-WiFi";
 const char* password = "Metallica";
 
 void setup() {
-  
+  Serial.begin(115200);
+  delay(10);
+  Serial.println('\n');
+
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to ");
+  Serial.print(ssid);
+  Serial.println(" ...");
+
+  int i=0;
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(++i);
+    Serial.print(' ');
+  }
+
+  Serial.println("...");
+  Serial.println('\n');
+  Serial.println("Connection established!");
+  Serial.print("IP address:\t");
+  Serial.println(WiFi.localIP());
 }
 
 void loop() {
